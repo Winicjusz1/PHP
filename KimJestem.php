28a7ad23db08d7c54b5e784293e472e2164c60bf
Winicjusz Cyboran <winicjuszcyboran@gmail.com>
2023-05-21 22:02:38 +0200

    Add files via upload

---
+++ KimJestem.php
@@ -0,0 +1,12 @@
+<?php
+// sprawdzenie czy plik jest częścią wywołania przez plik zawierający zmienną $add_page
+if (!$add_file)
+    exit;
+?>
+
+<div class="col-sm-6 bg-secondary text-dark font-weight-bold ml-2">
+    <h2>Witaj w moim portfolio.
+
+        Tu zamieszczamy linki do naszych zrealizowanych projektów.
+    </h2>
+</div>
